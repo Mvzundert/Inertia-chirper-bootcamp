e835cc85a3b1383cf3ee8cbefdab7705c576a63b
Marius van Zundert <marius.vanzundert@gmail.com>
2024-01-08 23:56:36 +0100

    Initial commit with Inertia bootcamp completed

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+use Illuminate\Http\RedirectResponse;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        return Inertia::render('Chirps/Index', [
+            'chirps' => Chirp::with('user:id,name')
+                ->latest()
+                ->get(),
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
+    }
+
+    /**
+     * Update a resource in storage
+     */
+    public function update(Request $request, Chirp $chirp): RedirectResponse
+    {
+        $this->authorize('update', $chirp);
+
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        $chirp->update($validated);
+
+        return redirect(route('chirps.index'));
+    }
+
+    public function destroy(Chirp $chirp): RedirectResponse
+    {
+        $this->authorize('delete', $chirp);
+
+        $chirp->delete();
+
+        return redirect(route('chirps.index'));
+    }
+}
---
+++ app/Policies/ChirpPolicy.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Chirp;
+use App\Models\User;
+
+class ChirpPolicy
+{
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Chirp $chirp): bool
+    {
+        return $chirp->user()->is($user);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Chirp $chirp): bool
+    {
+        return $this->update($user, $chirp);
+    }
+}
